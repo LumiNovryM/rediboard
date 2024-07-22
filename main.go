ca5033c4b65278b16a210c40472d2a8e30d3c448
fadhilma <fadhilmuhammadanshor@gmail.com>
2024-07-22 14:30:14 +0700

    fix: too many arguments in call to client.Gethave (context.Context, string)want (string)

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+	// "github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       15,
+	})
+
+	ctx := context.TODO()
+	client.Set(ctx, "language", "Go", 0)
+	language := client.Get(ctx, "language")
+	year := client.Get(ctx, "year")
+
+	fmt.Println(language.Val()) // "Go"
+	fmt.Println(year.Val())     // ""
+}
